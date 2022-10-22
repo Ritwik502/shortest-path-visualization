f3b3a33555497f7b5fde44b631f38dc6a9cd4a8a
Ritwik502 <84687323+Ritwik502@users.noreply.github.com>
2022-10-22 16:47:25 +0530

    Aligned text with box

--- frontend.py
+++ frontend.py
@@ -5,9 +5,9 @@ from tkinter import *
 root=Tk()
 root.title('Graph maker')
 root.configure(background="#03d7fc")
-root.geometry("600x600")
-inputt=Label(root,text='Entre edge data here in format\n (no. of nodes)\n(no. of edges)\nAre you using 1 based indexing[Y/N]\n(edge1)\n(edge2)\n.\n.\n.',background="#03d7fc")
-inputt.pack(pady=10)
+root.geometry("600x650")
+inputt=Label(root,text='Entre edge data here \n',font=("Arial", 25),background="#03d7fc")
+inputt.pack()
 cho=''
 
 def get_text():
@@ -17,14 +17,27 @@ def get_text():
         print(data,end='')
     root.destroy()
 my_text=[None]*4
-my_text[0]=Text(root,width=60,height=1)
-my_text[0].pack(pady=3)
-my_text[1]=Text(root,width=60,height=1)
-my_text[1].pack(pady=3)
-my_text[2]=Text(root,width=60,height=1)
-my_text[2].pack(pady=3)
-my_text[3]=Text(root,width=60,height=10)
-my_text[3].pack(pady=3)
+
+inputt=Label(root,text='(no. of nodes)',background="#03d7fc")
+inputt.pack()
+my_text[0]=Text(root,width=40,height=1)
+my_text[0].pack(pady=10)
+
+inputt=Label(root,text='(no. of edges)',background="#03d7fc")
+inputt.pack()
+my_text[1]=Text(root,width=40,height=1)
+my_text[1].pack(pady=10)
+
+inputt=Label(root,text='Are you using 1 based indexing[Y/N]',background="#03d7fc")
+inputt.pack()
+my_text[2]=Text(root,width=40,height=1)
+my_text[2].pack(pady=10)
+
+inputt=Label(root,text='Enter each edge as\n (node1 node2 edge_weight)',background="#03d7fc")
+inputt.pack()
+my_text[3]=Text(root,width=40,height=10)
+my_text[3].pack(pady=10)
+
 button_frame=Frame(root)
 button_frame.pack()
 get_text_b=Button(button_frame,text="Enter",command=get_text)
